3bba22735b58b3ff8f72f1d335aa1b34104d150b
NAnastasiyaaa <maherus16@gmail.com>
2022-09-29 21:37:33 +0300

    add cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const multer = require("multer");
 const path = require("path");
 
 dotenv.config();
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
